d1b496e690cfffcb73dab9cfa3a902f00d78e5b4
snd2cern_antonio <antonio.iuliano@cern.ch>
2023-04-25 13:49:36 +0200

    updating code for cell linking

--- condor_scripts/condorlinking_second.sh
+++ condor_scripts/condorlinking_second.sh
@@ -3,17 +3,17 @@
 ProcId=$2
 CELL=$3
 
-fromplate=11
-toplate=10
+fromplate=6
+toplate=6
 
 echo "Set up SND environment"
-SNDBUILD_DIR=/afs/cern.ch/work/a/aiulian/public/SNDLHCBuild/sw/
-source /cvmfs/sndlhc.cern.ch/SNDLHC-2022/July14/setUp.sh
+SNDBUILD_DIR=/afs/cern.ch/user/s/snd2cern/public/SNDBUILD/sw
+source /cvmfs/sndlhc.cern.ch/SNDLHC-2023/Jan22/setUp.sh
 eval `alienv load -w $SNDBUILD_DIR --no-refresh sndsw/latest-master-release`
-source /afs/cern.ch/work/a/aiulian/public/fedra/setup_new.sh
+source /afs/cern.ch/user/s/snd2cern/public/fedra/setup_new.sh
 
 echo  "go into reconstruction folder "
-cd /eos/experiment/sndlhc/emulsionData/2022/emureco_Napoli/RUN1_prova/b000021/
+cd /eos/experiment/sndlhc/emulsionData/2022/CERN/emu_reco/RUN1/b000044
 echo "do second linking for cell "$CELL
 echo "from plate " $fromplate
 echo "to plate " $toplate
