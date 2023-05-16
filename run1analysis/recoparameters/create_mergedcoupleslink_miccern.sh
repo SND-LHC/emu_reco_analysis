d66ae8545dc7b2b88fb44709b101dae0312a2093
snd2cern_antonio <antonio.iuliano@cern.ch>
2023-05-16 11:17:15 +0200

    using plate number options as usual for create symbolic link code

--- run1analysis/recoparameters/create_mergedcoupleslink_miccern.sh
+++ run1analysis/recoparameters/create_mergedcoupleslink_miccern.sh
@@ -1,6 +1,10 @@
 RECODATAPATH=/eos/experiment/sndlhc/emulsionData/2022/CERN/emu_reco/RUN1/b000044/
 BRICKID=44
-iplate=$1
-PLATEFOLDER="$(printf "p%0*d" 3 $iplate)"
 
-ln -s $RECODATAPATH/$PLATEFOLDER/$BRICKID.$1.0.0_merged.cp.root $RECODATAPATH/$PLATEFOLDER/$BRICKID.$1.0.0.cp.root
+for iplate in $(seq $2 $1)
+ 
+ do
+ PLATEFOLDER="$(printf "p%0*d" 3 $iplate)"
+ ln -s $RECODATAPATH/$PLATEFOLDER/$BRICKID.$iplate.0.0_merged.cp.root $RECODATAPATH/$PLATEFOLDER/$BRICKID.$iplate.0.0.cp.root
+
+ done
