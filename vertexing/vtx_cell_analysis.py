a658b8acfe9039043851135871e22a9f8c05bd64
snd2cern_antonio <antonio.iuliano@cern.ch>
2023-06-12 14:25:38 +0200

    one cell at time

--- vertexing/vtx_cell_analysis.py
+++ vertexing/vtx_cell_analysis.py
@@ -4,6 +4,14 @@ import fedrarootlogon
 from time import time
 import os.path
 
+from argparse import ArgumentParser
+parser = ArgumentParser()
+parser.add_argument("-x", dest="xcell", help="xcell", required=True, type=int)
+parser.add_argument("-y", dest="ycell", help="ycell", required=True, type=int)
+parser.add_argument("--clusID", dest="ClusterID", required=False, default=0)
+parser.add_argument("--procID", dest="ProcID", required=False, default=None, type=int)
+options = parser.parse_args()
+
 brickID = 21
 from_plate = 57
 to_plate = 1
@@ -20,25 +28,34 @@ out_dir = '/eos/experiment/sndlhc/emulsionData/2022/emureco_Napoli/RUN1/b{:06d}/
 #histo setup
 h_n = r.TH1D('n','multiplicity;multiplicity', 50, 0, 50)
 h_flag = r.TH1D('flag','flag;flag', 6, 0, 6)
-h_vz = r.TH1D('vz','vz;vz[um]', 400, -80000, 0)
+h_vz = r.TH1D('vz','vz;vz[um]', 400, -80000, 5000)
 h_vxy = r.TH2D('vxvy', 'vxvy;vx[um];vy[um]', 190, 0, 190000, 190, 190000)
-h_n0 = r.TH1D('ntracks', 'multiplicity;multiplicity', 48, 2.5, 50.5)
-h_nseg = r.TH1D('nseg', 'nseg;nseg', 32, 0, 32)
-h_npl = r.TH1D('npl', 'npl;npl', 32, 0, 32)
-h_ff = r.TH1D('ff', 'fill factor;FF', 22, -0.05, 1.05)
-h_imp = r.TH1D('ip', 'impact parameter;ip[um]', 50, -0.5, 150)
-h_prob = r.TH1D('vtx_prob', 'probability;prob', 30, 0, 1.1)
-h_maxape = r.TH1D('max_aperure', 'max aperture;max_ape', 200, 0, 200)
+h_n0 = r.TH1D('ntracks', 'multiplicity;multiplicity', 47, 3, 50)
+h_nseg = r.TH1D('nseg', 'nseg;nseg', 52, 4, 58)
+h_npl = r.TH1D('npl', 'npl;npl', 58, 0, 58)
+h_ff = r.TH1D('ff', 'fill factor;FF', 22, 0, 1.1)
+h_imp = r.TH1D('ip', 'impact parameter;ip[um]', 100, 0, 300)
+h_prob = r.TH1D('prob', 'probability;prob', 30, 0, 1.05)
+h_maxape = r.TH1D('maxape', 'max aperture;max_ape', 100, 0, 5)
+
+xbin=options.xcell
+ybin=options.ycell
 
 #save vertices in root file
-outputFile = r.TFile(out_dir+"/vertex_selection.root","RECREATE")	
+outputFile = r.TFile(out_dir+"/vertex_selection_{}_{}.root".format(xbin, ybin),"RECREATE")	
 # outputTree = r.TNtuple("vtx","Tree of vertices","cell:vID:vx:vy:vz:flag:n:nseg:npl:nfirst:ff:meanIP:prob:maxape")
 outputTree = r.TNtuple("vtx","Tree of vertices","cell:vID:n:ff:meanIP:prob:maxape")
 
-for icell in range(362):
+meminfo = r.MemInfo_t()
+info = r.gSystem.GetMemInfo(meminfo)
+print("memory start", meminfo.fMemUsed)
+
+for icell in range(1):
     start_time = time()
-    xbin=((icell // ncells)) + 1
-    ybin=((icell % ncells)) + 1
+    #xbin=((icell // ncells)) + 1
+    #ybin=((icell % ncells)) + 1
+    xbin=options.xcell
+    ybin=options.ycell
     cell_path = path+'/cell_{}'.format(ybin)
     #reading vertices
     vtx_file = cell_path+'/vertextree_{}_{}.root'.format(xbin, ybin)
@@ -66,23 +83,24 @@ for icell in range(362):
     for vtx in vertices:
         vx = vtx.VX()
         vy = vtx.VY()
-        vz = vtx.VZ()
-        flag = vtx.Flag()
-        ntracks = vtx.N()
         #cell cuts
         if vx < (xbin-1-overlap_fraction/2)*cellsize: continue
         if vx > (xbin+overlap_fraction/2)*cellsize: continue
         if vy < (ybin-1-overlap_fraction/2)*cellsize: continue
         if vy > (ybin+overlap_fraction/2)*cellsize: continue
-        h_n.Fill(ntracks)
-        h_flag.Fill(flag)
+        vz = vtx.VZ()
+        flag = vtx.Flag()
+        ntracks = vtx.N()
         h_vz.Fill(vz)
-        #soft cuts
-        if ntracks < 3: continue
+        #z cut
         if vz < zmin: continue
         if vz > 0: continue
+        h_n.Fill(ntracks)
+        #multiplicity cut
+        if ntracks < 3: continue
+        h_flag.Fill(flag)
         h_vxy.Fill(vx, vy)
-        #flag 0 study
+        #flag 0 cut
         if flag == 0 or flag == 3:
             h_n0.Fill(ntracks)
             nplList = []
@@ -109,6 +127,8 @@ for icell in range(362):
             # outputTree.Fill(icell, vtx.ID(), vx, vy, vz, flag, ntracks, np.mean(nsegList), np.mean(nplList), nfirst, np.mean(ffList), np.mean(ipList), vtx.V().prob(), vtx.MaxAperture())
             outputTree.Fill(icell, vtx.ID(), ntracks, np.mean(ffList), np.mean(ipList), vtx.V().prob(), vtx.MaxAperture())
     del gAli
+    info = r.gSystem.GetMemInfo(meminfo)
+    print("memory end", meminfo.fMemUsed)
     print(time()-start_time)
 
 
@@ -117,7 +137,7 @@ outputFile.cd()
 outputTree.Write()
 outputFile.Close()
 
-histoFile = r.TFile(out_dir+"/hist_out.root", "RECREATE")
+histoFile = r.TFile(out_dir+"/hist_out_{}_{}.root".format(xbin, ybin), "RECREATE")
 h_n.Write()
 h_flag.Write()
 h_vz.Write()
