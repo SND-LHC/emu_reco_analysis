feb234d8ef29ea33c001c233ef534d14df020303
scanner <antonio.iuliano@unina.it>
2023-09-27 14:17:32 +0200

    adding unit to axis

--- quality_checks/CheckFrames.C
+++ quality_checks/CheckFrames.C
@@ -6,13 +6,21 @@ void CheckFrames()
   c->cd(1)->SetGrid();
   FrameAlign->Draw("dx:z1", cut && "side==0","");
   ((TH2F*)gPad->GetPrimitive("htemp"))->GetYaxis()->SetLimits(-1,1);
+  ((TH2F*)gPad->GetPrimitive("htemp"))->GetXaxis()->SetTitle("z1[#mum]");
+  ((TH2F*)gPad->GetPrimitive("htemp"))->GetYaxis()->SetTitle("dx[#mum]");
   c->cd(2)->SetGrid();
   FrameAlign->Draw("dx:z1", cut &&"side==1","");
   ((TH2F*)gPad->GetPrimitive("htemp"))->GetYaxis()->SetLimits(-1,1);
+  ((TH2F*)gPad->GetPrimitive("htemp"))->GetXaxis()->SetTitle("z1[#mum]");
+  ((TH2F*)gPad->GetPrimitive("htemp"))->GetYaxis()->SetTitle("dx[#mum]");
   c->cd(3)->SetGrid();
   FrameAlign->Draw("dy:z1", cut && "side==0","");
   ((TH2F*)gPad->GetPrimitive("htemp"))->GetYaxis()->SetLimits(-1.5,1.5);
+  ((TH2F*)gPad->GetPrimitive("htemp"))->GetXaxis()->SetTitle("z1[#mum]");
+  ((TH2F*)gPad->GetPrimitive("htemp"))->GetYaxis()->SetTitle("dy[#mum]");
   c->cd(4)->SetGrid();
   FrameAlign->Draw("dy:z1", cut && "side==1","");
   ((TH2F*)gPad->GetPrimitive("htemp"))->GetYaxis()->SetLimits(-1.5,1.5);
+  ((TH2F*)gPad->GetPrimitive("htemp"))->GetXaxis()->SetTitle("z1[#mum]");
+  ((TH2F*)gPad->GetPrimitive("htemp"))->GetYaxis()->SetTitle("dy[#mum]");
 }
