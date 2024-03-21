58d1500f37ca3129c14a70a188643d8249901622
Oliver Lantwin <oliver.lantwin@cern.ch>
2024-03-21 16:12:22 +0100

    Update transfer script to use xrdcp
    Changes based on version suggested by Gerardo on mattermost

--- transfertoEOS_RUN3_MIC2.sh
+++ transfertoEOS_RUN3_MIC2.sh
@@ -1,10 +1,9 @@
-#example: rsync --stats -rlptDvxW  /drives/d/SND/RUN3/ snd2cern@lxplus.cern.ch:/eos/experiment/sndlhc/emulsionData/2022/CERN/SND/RUN3/ &> /drives/d/rsync_logs/2022_24_11_rsinc_cern2eos.log
+#!/bin/bash
 
-#!/bin/sh
+RUN=3
+remotedir="/eos/experiment/sndlhc/emulsionData/2022/CERN/SND/RUN${RUN}/"
+currdate=$(date +"%Y%m%d_%s")
+cryptedfile="/drives/d/snd2cern.txt.gpg"
 
-localdir=/drives/d/SND/RUN3/RUN3_W5_B1
-remotedir=/eos/experiment/sndlhc/emulsionData/2022/CERN/SND_mic2/RUN3/
-currdate=`date +"%Y%m%d_%s"`
-cryptedfile=/drives/d/snd2cern.txt.gpg
-
-gpg -d -q $cryptedfile | sshpass rsync --stats -rlptDvxW ${localdir} --exclude '*.obx' snd2cern@lxplus.cern.ch:${remotedir} &> /drives/d/rsync_logs/${currdate}_rsync_cern2eos.log
+# Decrypt the encrypted file
+gpg -d -q "${cryptedfile}" | xrdcp -N -v -s -f - -snd2cern@lxplus.cern.ch:"${remotedir}" &> "/drives/d/rsync_logs/${currdate}_xrdcp_cern2eos.log"
